81376572d4e12904d47f965fd0a5f12f8806e637
nicolas-dealmeida <ndealmeida@la-providence.net>
2021-09-16 11:46:38 +0200

    JS.

--- chat.js
+++ chat.js
@@ -1,7 +1,21 @@
-// API
+    // API
 
-// Chat JS
+    // Chat JS
 
-// Objets
+    // Objets
 
-// Fonctions
+    // Fonctions
+    function Affichage(AuthChat){
+        if(AuthChat == 1){
+            document.getElementById('AuthOn').className = "Sous-Menu-1 DisplayON";
+            document.getElementById('AuthOff').className = "Sous-Menu-1 DisplayOFF";
+            document.getElementById('AuthChatOn').className = "Chat DisplayON";
+            document.getElementById('AuthChatOff').className = "Chat DisplayOFF";
+        }
+        else{
+            document.getElementById('AuthOn').className = "Sous-Menu-1 DisplayOFF";
+            document.getElementById('AuthOff').className = "Sous-Menu-1 DisplayON";
+            document.getElementById('AuthChatOn').className = "Chat DisplayOFF";
+            document.getElementById('AuthChatOff').className = "Chat DisplayON";
+        }
+    }
