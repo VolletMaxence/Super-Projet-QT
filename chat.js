e3359581ec93513c07a1d5ed42ac5858cad301a4
nicolas-dealmeida <ndealmeida@la-providence.net>
2021-09-16 14:42:04 +0200

    Mise à jours.

--- chat.js
+++ chat.js
@@ -1,3 +1,6 @@
+    // Variables
+    var AuthChat;
+
     // API
 
     // Chat JS
@@ -5,17 +8,20 @@
     // Objets
 
     // Fonctions
-    function Affichage(AuthChat){
-        if(AuthChat == 1){
-            document.getElementById('AuthMenuOn').className = "Sous-Menu-1 DisplayON";
-            document.getElementById('AuthMenuOff').className = "Sous-Menu-1 DisplayOFF";
-            document.getElementById('AuthChatOn').className = "Chat DisplayON";
-            document.getElementById('AuthChatOff').className = "Chat DisplayOFF";
+        function Affichage(AuthChat){
+            if(AuthChat == 1){
+                document.getElementById('AuthMenuOn').className = "Sous-Menu-1 DisplayON";
+                document.getElementById('AuthMenuOff').className = "Sous-Menu-1 DisplayOFF";
+                document.getElementById('AuthChatOn').className = "Chat DisplayON";
+                document.getElementById('AuthChatOff').className = "Chat DisplayOFF";
+            }
+            else{
+                document.getElementById('AuthMenuOn').className = "Sous-Menu-1 DisplayOFF";
+                document.getElementById('AuthMenuOff').className = "Sous-Menu-1 DisplayON";
+                document.getElementById('AuthChatOn').className = "Chat DisplayOFF";
+                document.getElementById('AuthChatOff').className = "Chat DisplayON";
+            }
         }
-        else{
-            document.getElementById('AuthMenuOn').className = "Sous-Menu-1 DisplayOFF";
-            document.getElementById('AuthMenuOff').className = "Sous-Menu-1 DisplayON";
-            document.getElementById('AuthChatOn').className = "Chat DisplayOFF";
-            document.getElementById('AuthChatOff').className = "Chat DisplayON";
+        function AffichageStart(){
+            Affichage(0);
         }
-    }
