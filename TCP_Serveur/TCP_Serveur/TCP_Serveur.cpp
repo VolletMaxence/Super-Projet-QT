f8e4a247e973829ace752bac306b43048a4df585
ColsonB <bcolson@la-providence.net>
2021-09-16 11:49:15 +0200

    Commit

--- TCP_Serveur/TCP_Serveur/TCP_Serveur.cpp
+++ TCP_Serveur/TCP_Serveur/TCP_Serveur.cpp
@@ -2,9 +2,8 @@
 
 TCP_Serveur::TCP_Serveur(QWidget *parent)
     : QMainWindow(parent)
+
 {
-    ui.setupUi(this);
-	
 	server = new QTcpServer(this);
 	QObject::connect(server, SIGNAL(newConnection()), this, SLOT(onServerNewConnection()));
 	server->listen(QHostAddress::AnyIPv4, 4321);
@@ -12,7 +11,7 @@ TCP_Serveur::TCP_Serveur(QWidget *parent)
 
 void TCP_Serveur::onServerNewConnection()
 {
-	ui.connectionStatusLabel->setText("Un client c'est connecté");
+	ui.connectionStatusLabel->setText("Un client s'est connecté");
 	QTcpSocket * client = server->nextPendingConnection();
 	QObject::connect(client, SIGNAL(readyRead()), this, SLOT(onClientReadyRead()));
 	QObject::connect(client, SIGNAL(disconnect()), this, SLOT(onClientDisconnected()));
