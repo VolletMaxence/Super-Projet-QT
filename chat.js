58d3b735d790c63c5f8dedeec2c6ca2ba3aa9595
nicolas-dealmeida <ndealmeida@la-providence.net>
2021-09-16 13:29:11 +0200

    Correction.

--- chat.js
+++ chat.js
@@ -7,14 +7,14 @@
     // Fonctions
     function Affichage(AuthChat){
         if(AuthChat == 1){
-            document.getElementById('AuthOn').className = "Sous-Menu-1 DisplayON";
-            document.getElementById('AuthOff').className = "Sous-Menu-1 DisplayOFF";
+            document.getElementById('AuthMenuOn').className = "Sous-Menu-1 DisplayON";
+            document.getElementById('AuthMenuOff').className = "Sous-Menu-1 DisplayOFF";
             document.getElementById('AuthChatOn').className = "Chat DisplayON";
             document.getElementById('AuthChatOff').className = "Chat DisplayOFF";
         }
         else{
-            document.getElementById('AuthOn').className = "Sous-Menu-1 DisplayOFF";
-            document.getElementById('AuthOff').className = "Sous-Menu-1 DisplayON";
+            document.getElementById('AuthMenuOn').className = "Sous-Menu-1 DisplayOFF";
+            document.getElementById('AuthMenuOff').className = "Sous-Menu-1 DisplayON";
             document.getElementById('AuthChatOn').className = "Chat DisplayOFF";
             document.getElementById('AuthChatOff').className = "Chat DisplayON";
         }
