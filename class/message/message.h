d2914f913d8cfe809c5de5a511af93a16b43dc3f
kylian-duval <kduval@la-providence.net>
2021-09-21 10:58:26 +0200

    ajout de la class message

---
+++ class/message/message.h
@@ -0,0 +1,14 @@
+#pragma once
+class message
+{
+private : 
+	int ID_message;
+	char Date;
+	char Contenu;
+	int ID_Utilisateur;
+
+public:
+	//fonction qui permet de envoyer un message elle prend en paramettre char Contenu qui est le messgae et le IDCompte qui est le expediteur 
+	void EnvoieMessage(char Contenu, int IDCompte)
+};
+
