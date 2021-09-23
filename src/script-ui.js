3094d0155cdac92f7e6f4b2fc8e8883ef8ce2cff
nicolas-dealmeida <ndealmeida@la-providence.net>
2021-09-23 09:52:35 +0200

    Ajout Différence Connexion/Inscription.

--- src/script-ui.js
+++ src/script-ui.js
@@ -1,137 +1,160 @@
 $(function(){
-    var dialog, form,
-
     //Value Inscription
-        name = $("#in-name"),
-        password = $("#in-password"),
-        allFields = $([]).add(name).add(password),
-        tips = $(".validateTips");
+        var in_form;
+        var in_dialog;
+        var in_name = $("#in_name");
+        var in_password = $("#in_password");
+        var in_allFields = $([]).add(in_name).add(in_password);
+        var in_tips = $(".in_validateTips");
     //Value Connexion
-        name = $("#co-name"),
-        password = $("#co-password"),
-        allFields = $([]).add(name).add(password),
-        tips = $(".validateTips");
-
-    // Fonction Check Global
-    function updateTips(t){
-        tips.text(t).addClass("ui-state-highlight");
-        setTimeout(function(){
-            tips.removeClass("ui-state-highlight", 1500);
-        }, 500);
-    }
-    function checkLength(o, n, min, max){
-        if(o.val().length > max || o.val().length < min){
-            o.addClass("ui-state-error");
-            updateTips("Longueur de " + n + " doit être entre " +
-            min + " et " + max + ".");
-            return false;
-        }else{
-            return true;
-        }
-    }
-    function checkRegexp(o, regexp, n){
-        if(!(regexp.test(o.val()))){
-            o.addClass("ui-state-error");
-            updateTips(n);
-            return false;
-        }else{
-            return true;
-        }
-    }
+        var co_form;
+        var co_dialog;
+        var co_name = $("#co_name");
+        var co_password = $("#co_password");
+        var co_allFields = $([]).add(co_name).add(co_password);
+        var co_tips = $(".co_validateTips");
 
     // Fonction Suivant Inscription Bouton
-        function addUser(){
+        function IN_User(){
             var valid = true;
-            allFields.removeClass("ui-state-error");
+            in_allFields.removeClass("ui-state-error");
 
-            valid = valid && checkLength(name, "username", 3, 16);
-            valid = valid && checkLength(password, "password", 5, 16);
+            valid = valid && in_checkLength(in_name, "username", 3, 16);
+            valid = valid && in_checkLength(in_password, "password", 5, 16);
 
-            valid = valid && checkRegexp(name, /^[a-z]([0-9a-z_\s])+$/i, "Le nom d'utilisateur ne peut-être que composé de chiffres, de lettres, d'underscores, d'espaces, et doit commencer par une lettre.");
-            valid = valid && checkRegexp(password, /^([0-9a-zA-Z])+$/, "Le mot de passe n'autorise que des chiffres et des lettres.");
+            valid = valid && in_checkRegexp(in_name, /^[a-z]([0-9a-z_\s])+$/i, "Le nom d'utilisateur ne peut-être que composé de chiffres, de lettres, d'underscores, d'espaces, et doit commencer par une lettre.");
+            valid = valid && in_checkRegexp(in_password, /^([0-9a-zA-Z])+$/, "Le mot de passe n'autorise que des chiffres et des lettres.");
 
             if(valid){
-                var Pseudo      =   name.val();
-                var PassWord    =   password.val();
-                dialog.dialog("close");
-                window.alert('Le Pseudo est \"' + Pseudo + '\" et le Mots de Passe est \"'+ PassWord + '\".')
-                //  LOGIN       : "LOGIN :: Pseudo :: Pseudo MdP : PassWord "
-            }
+                var IN_Pseudo   =   in_name.val();
+                var IN_PassWord =   in_password.val();
+                in_dialog.dialog("close");
+                window.alert('Le Pseudo est \"' + IN_Pseudo + '\" et le Mots de Passe est \"'+ IN_PassWord + '\".');
+                window.alert('INSCRIPTION : "INSCRIPTION :: Pseudo :: IN_Pseudo MdP : IN_PassWord "');
+            };
             return valid;
-        }
+        };
 
     // Fonction Suivant Connection Bouton
-        function CoUser(){
+        function Co_User(){
             var valid = true;
-            allFields.removeClass("ui-state-error");
+            co_allFields.removeClass("ui-state-error");
 
-            valid = valid && checkLength(name, "username", 3, 16);
-            valid = valid && checkLength(password, "password", 5, 16);
+            valid = valid && co_checkLength(co_name, "username", 3, 16);
+            valid = valid && co_checkLength(co_password, "password", 5, 16);
 
-            valid = valid && checkRegexp(name, /^[a-z]([0-9a-z_\s])+$/i, "Le nom d'utilisateur ne peut-être que composé de chiffres, de lettres, d'underscores, d'espaces, et doit commencer par une lettre.");
-            valid = valid && checkRegexp(password, /^([0-9a-zA-Z])+$/, "Le mot de passe n'autorise que des chiffres et des lettres.");
+            valid = valid && co_checkRegexp(co_name, /^[a-z]([0-9a-z_\s])+$/i, "Le nom d'utilisateur ne peut-être que composé de chiffres, de lettres, d'underscores, d'espaces, et doit commencer par une lettre.");
+            valid = valid && co_checkRegexp(co_password, /^([0-9a-zA-Z])+$/, "Le mot de passe n'autorise que des chiffres et des lettres.");
 
             if(valid){
-                var Pseudo      =   name.val();
-                var PassWord    =   password.val();
-                dialog.dialog("close");
-                window.alert('Le Pseudo est \"' + Pseudo + '\" et le Mots de Passe est \"'+ PassWord + '\".')
-                //  LOGIN       : "LOGIN :: Pseudo :: Pseudo MdP : PassWord "
-            }
+                var CO_Pseudo   =   co_name.val();
+                var CO_PassWord =   co_password.val();
+                co_dialog.dialog("close");
+                window.alert('Le Pseudo est \"' + CO_Pseudo + '\" et le Mots de Passe est \"'+ CO_PassWord + '\".');
+                window.alert('LOGIN : "LOGIN :: Pseudo :: IN_Pseudo MdP : IN_PassWord "');
+            };
             return valid;
-        }
+        };
 
     // Fonction sur la Boite Dialogue Inscription
-        dialog = $("#dialog-inscription").dialog({
+        in_dialog = $("#dialog_inscription").dialog({
             autoOpen: false,
             height: 350,
             width: 350,
             modal: true,
             buttons:{
-                "Créer un compte": addUser,
+                "Créer un compte": IN_User,
                 Cancel: function(){
-                    dialog.dialog("close");
+                    in_dialog.dialog("close");
                 }
             },
             close: function(){
-                form[0].reset();
-                allFields.removeClass("ui-state-error");
+                in_form[0].reset();
+                in_allFields.removeClass("ui-state-error");
             }
         });
-
-        form = dialog.find("form").on("submit", function(event){
+        in_form = in_dialog.find("form").on("submit", function(event){
             event.preventDefault();
-            addUser();
+            IN_User();
         });
 
-        $("#create-user").button().on("click", function(){
-            dialog.dialog("open");
+        $("#create_user").button().on("click", function(){
+            in_dialog.dialog("open");
         });
+        function in_updateTips(t){
+            in_tips.text(t).addClass("ui-state-highlight");
+            setTimeout(function(){
+                in_tips.removeClass("ui-state-highlight", 1500);
+            }, 500);
+        };
+        function in_checkLength(o, n, min, max){
+            if(o.val().length > max || o.val().length < min){
+                o.addClass("ui-state-error");
+                in_updateTips("Longueur de " + n + " doit être entre " +
+                min + " et " + max + ".");
+                return false;
+            }else{
+                return true;
+            }
+        };
+        function in_checkRegexp(o, regexp, n){
+            if(!(regexp.test(o.val()))){
+                o.addClass("ui-state-error");
+                in_updateTips(n);
+                return false;
+            }else{
+                return true;
+            }
+        };
 
     // Fonction sur la Boite Dialogue Connexion
-        dialog = $("#dialog-connect").dialog({
+        co_dialog = $("#dialog_connect").dialog({
             autoOpen: false,
             height: 350,
             width: 350,
             modal: true,
             buttons:{
-                "Se connecter": CoUser,
+                "Se connecter": Co_User,
                 Cancel: function(){
-                    dialog.dialog("close");
+                    co_dialog.dialog("close");
                 }
             },
             close: function(){
-                form[0].reset();
-                allFields.removeClass("ui-state-error");
+                co_form[0].reset();
+                co_allFields.removeClass("ui-state-error");
             }
         });
-
-        form = dialog.find("form").on("submit", function(event){
+        co_form = co_dialog.find("form").on("submit", function(event){
             event.preventDefault();
-            addUser();
+            CO_User();
         });
 
-        $("#connect-user").button().on("click", function(){
-            dialog.dialog("open");
+        $("#connect_user").button().on("click", function(){
+            co_dialog.dialog("open");
         });
+        function co_updateTips(t){
+            co_tips.text(t).addClass("ui-state-highlight");
+            setTimeout(function(){
+                co_tips.removeClass("ui-state-highlight", 1500);
+            }, 500);
+        };
+        function co_checkLength(o, n, min, max){
+            if(o.val().length > max || o.val().length < min){
+                o.addClass("ui-state-error");
+                co_updateTips("Longueur de " + n + " doit être entre " +
+                min + " et " + max + ".");
+                return false;
+            }else{
+                return true;
+            }
+        };
+        function co_checkRegexp(o, regexp, n){
+            if(!(regexp.test(o.val()))){
+                o.addClass("ui-state-error");
+                co_updateTips(n);
+                return false;
+            }else{
+                return true;
+            }
+        };
 });
