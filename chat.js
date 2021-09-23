d4143614f4c924e8a89cc4ba433d936d2d12ba48
nicolas-dealmeida <ndealmeida@la-providence.net>
2021-09-23 11:13:06 +0200

    CSS

--- chat.js
+++ chat.js
@@ -8,20 +8,19 @@
     // Fonctions
         function Affichage(AuthChat){
             if(AuthChat == 1){
-                document.getElementById('AuthMenuOn').className = "Sous-Menu-1 DisplayON";
-                document.getElementById('AuthMenuOff').className = "Sous-Menu-1 DisplayOFF";
+                document.getElementById('unlogin_user').className = "Sous-Menu-1 DisplayON";
+                document.getElementById('connect_user').className = "Sous-Menu-1 DisplayOFF";
+                document.getElementById('create_user').className = "Sous-Menu-1 DisplayOFF";
                 document.getElementById('AuthChatOn').className = "Chat DisplayON";
                 document.getElementById('AuthChatOff').className = "Chat DisplayOFF";
             }
             else{
-                document.getElementById('AuthMenuOn').className = "Sous-Menu-1 DisplayOFF";
-                document.getElementById('AuthMenuOff').className = "Sous-Menu-1 DisplayON";
+                document.getElementById('unlogin_user').className = "Sous-Menu-1 DisplayOFF";
+                document.getElementById('connect_user').className = "Sous-Menu-1 DisplayON";
+                document.getElementById('create_user').className = "Sous-Menu-1 DisplayON";
                 document.getElementById('AuthChatOn').className = "Chat DisplayOFF";
                 document.getElementById('AuthChatOff').className = "Chat DisplayON";
             }
-        }
-        function AffichageStart(){
-            Affichage(0);
         }
         // Fonction contact Serveur
         function ContactServer(content){
