e28f05c0b2396470f9ea90c3dfc1ddb0abeb4170
kylian-duval <70692979+kylian-duval@users.noreply.github.com>
2021-10-03 17:55:08 +0200

    Update sketch.js

--- websocket/sketch.js
+++ websocket/sketch.js
@@ -2,7 +2,17 @@ var socket = null;
 
 socket = new WebSocket("ws://127.0.0.1:4321");
 
+//fonction qui permet d'envoyer un message au server tcp
 function Message(){
     var message = document.getElementById('message').value;
     socket.send(message);
+}
+
+//fonction qui permet de s'inscrire
+function inscriptin(){
+
+}
+//fonction qui permet de se connecter
+function connection(){
+
 }
