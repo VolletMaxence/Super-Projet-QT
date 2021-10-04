6b793fae793fcf09e04cabf0d96c272e7154e736
kylian-duval <70692979+kylian-duval@users.noreply.github.com>
2021-10-04 18:47:31 +0200

    update

--- websocket/socket_client.js
+++ websocket/socket_client.js
@@ -1,12 +1,16 @@
 var socket = null;
 
-socket = new WebSocket("ws://127.0.0.1:4321");
+//socket = new WebSocket("ws://127.0.0.1:4321");
 
 //fonction qui permet d'envoyer un message au server tcp
+
 function Message(){
     var message = document.getElementById('message').value;
+    document.getElementById('message').value = "";
     socket.send(message);
+    afficheMess(message);
 }
+
 
 //fonction qui permet de s'inscrire
 function inscriptin(){
@@ -14,5 +18,13 @@ function inscriptin(){
 }
 //fonction qui permet de se connecter
 function connection(){
+
+}
+
+
+function afficheMess(Message){
+    var li = document.createElement('p');
+    li.innerText = Message;
+    document.getElementById("Mess").appendChild(li);
 
 }
